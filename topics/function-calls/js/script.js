eb88749e94594c82939135524057dafd2af72350
Biwanka <gauthier.bibi10@gmail.com>
2024-09-11 16:24:05 -0400

    added comments to the blank page project

--- topics/function-calls/js/script.js
+++ topics/function-calls/js/script.js
@@ -1,24 +1,35 @@
 /**
- * Title of Project
+ * The Blank Page
  * Bianca Gauthier
  * 
- * Working on function calls examples
+ * Exploration of the existential angst of a novelist 
+ * who must sit down at their pink desk and confront the 
+ * abyss that is a blank page a paper.
+ * 
+ * The progamme is non-interactive to convey the inability 
+ * to get started on the project. Try typing! Just try it!
  */
 
 "use strict";
 
+/**
+ *create canvas for our masterpieces
+*/
 
 function setup() {
-    // Once at the beginning of the program
+    // Create the canvas at a standrad resolution
     createCanvas (640, 480);
 
 }
 
-
+/**
+ * Draws the writer's desktop and a blank piece of paper 
+ */
 
 function draw() {
-    // Every Frame
+    // The pink desktop
     background(255, 100, 100);
+    //The blank piece of paper
     rect(200, 80, 240, 320);
 
 
