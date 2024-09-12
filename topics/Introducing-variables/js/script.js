5865bb0e5253407aa644f9ee1e53195815726e2c
Biwanka <gauthier.bibi10@gmail.com>
2024-09-12 17:45:10 -0400

    Started the introducing variables project

---
+++ topics/Introducing-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing Variable
+ * Bianca Gauthier
+ * 
+ * Learning what a variable is and does!
+ */
+
+"use strict";
+
+/**
+ * Create a canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
