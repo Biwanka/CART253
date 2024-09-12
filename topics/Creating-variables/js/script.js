cc1e8503894f018cfe2a35923199cefcd27c8978
Biwanka <gauthier.bibi10@gmail.com>
2024-09-12 18:19:10 -0400

    drew a hole in a piece a cheese

--- topics/Creating-variables/js/script.js
+++ topics/Creating-variables/js/script.js
@@ -1,24 +1,35 @@
 /**
- * Title of Project
- * Author Name
+ * Creating Variables
+ * Bianca Gauthier
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Experimenting with creating variables 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * create the canvas
 */
 function setup() {
+    createCanvas(480, 480);
+
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a hole in a piece of cheese
 */
 function draw() {
+
+    //the cheese
+    background(255, 255, 0);
+
+    //the hole
+    push();
+    noStroke();
+    fill(0);
+    ellipse(140, 175, 180);
+    pop();
 
 }
