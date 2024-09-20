15ffc60bb82a38c4a63cd6c91faca7784c2f20ba
Biwanka <gauthier.bibi10@gmail.com>
2024-09-20 17:06:07 -0400

    started the movement project module

---
+++ topics/movement/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Movement
+ * Bianca gauthier
+ * 
+ * Experimenting with movement
+ */
+
+"use strict";
+
+/**
+ * 
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
