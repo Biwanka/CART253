42c960dfef645c8df72f2168d17b5cf14ce7e83f
Biwanka <gauthier.bibi10@gmail.com>
2024-10-02 16:43:15 -0400

    made sure that everything had comments

--- topics/ArtJam_Bianca_Gauthier/js/script.js
+++ topics/ArtJam_Bianca_Gauthier/js/script.js
@@ -2,7 +2,9 @@
  * Cat PLay Time
  * Bianca Gauthier
  * 
- * The purpose of this is to recreate the idea of a cat that is plying with a laserPointer.
+ * This project put players in the position of a cat. It demonstrate a cat that is palying with a laserPointer. 
+ * clicking the laserPointer will make it move to a different randomized area. The floor also chnages colour with the mousse 
+ * movement to reacreat the idea if the cat playing in different eareas and is running fast.
  */
 
 "use strict";
@@ -18,9 +20,6 @@ let catPaw = {
     image: undefined
 };
 
-let floorColour = {
-    fill: undefined
-};
 
 // the red dot from a laser pointer. The one that the cat is trying to catch 
 const laserPointer = {
@@ -32,10 +31,7 @@ const laserPointer = {
 
 
 };
-/**The backgroun will change colour to give
-    *the feeling that the cat is chasing the laser pointer in different 
-    *rooms 
-    */
+//the floor
 const floor = {
     x: 400,
     y: 300,
@@ -50,7 +46,7 @@ function preload() {
 
 }
 
-
+//have created the canvas
 function setup() {
     createCanvas(800, 600);
     noCursor();
@@ -58,7 +54,7 @@ function setup() {
 
 }
 
-
+//has all of my different functions to make everything work
 function draw() {
 
     background(100);
@@ -83,36 +79,47 @@ function moveCatPaw() {
     catPaw.x = mouseX;
     catPaw.y = mouseY;
 }
-
+//The function that makes the LaserPointer move to random positions 
+//also the function that makes the laser pointer move only when it is pressed.
 function checkInput() {
 
+    //claculating the distance so the circle only moves when the cat paw clicks on it.
     const distance = dist(catPaw.x, catPaw.y, laserPointer.x, laserPointer.y);
     const mouseOverlapsLaserPointer = (distance < laserPointer.size / 2);
-
+    //if function so if the catpaw, is over the laserpointer and presses the laserpointer
+    //then the red circle whihc is the laserpointer will appear somewhere random on the floor
+    //this recreates the effect of a cat playing with a laserpointer.
     if (mouseOverlapsLaserPointer && mouseIsPressed) {
         laserPointer.x = random(0, width);
         laserPointer.y = random(0, height);
     }
 
-
+    //if the laserpointer is not pressed then it will not move
     else {
         laserPointer.x = laserPointer.x;
         laserPointer.y = laserPointer.y;
     }
 
 }
-
+/**this makes the The backgroun change colour to give
+    *the feeling that the cat is chasing the laser pointer in different 
+    *rooms.
+    */
 function drawFloorColour() {
+
+    //calculates so the change in colour only happens when the catpaw is on the floor
+    //i used a circle so there are some extra that is reacted when it goes over the canvas.
     const distance = dist(catPaw.x, catPaw.y, floor.x, floor.y);
     const mouseOverlapsFloor = (distance < floor.size / 2);
     const mouseIsMoving = (movedX !== 0 || movedY !== 0);
-
+    //if function so if the mouse is moving then the colour of the floor will change with it.
     if (mouseOverlapsFloor && mouseIsMoving) {
         drawFloor.fill = map(mouseX, 0, 100, 0, 100);
         drawFloor.fill = map(mouseY, 0, 100, 100, 0);
     }
 }
 
+//this draws the floor that the cat is playing on
 
 function drawFloor() {
     push();
@@ -122,7 +129,7 @@ function drawFloor() {
     pop();
 }
 
-// Draws the red circle laser pointer that the cat is trying to catch 
+// Draws the red circle whihc represent a laser pointer that the cat is trying to catch 
 function drawLaserPointer() {
     push();
     noStroke();
